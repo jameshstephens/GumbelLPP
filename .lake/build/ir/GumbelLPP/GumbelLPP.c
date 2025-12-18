a0cbf8ba4aa74b82a9624cb9da797c48933e6b5c
James Stephens <jameshstephens3@gmail.com>
2025-12-18 10:39:02 -0600

    make it tiny

--- .lake/build/ir/GumbelLPP/GumbelLPP.c
+++ .lake/build/ir/GumbelLPP/GumbelLPP.c
@@ -1,6 +1,6 @@
 // Lean compiler output
 // Module: GumbelLPP.GumbelLPP
-// Imports: public import Init public import Mathlib.Analysis.SpecialFunctions.ExpDeriv public import Mathlib.Analysis.SpecialFunctions.Log.Deriv public import Mathlib.Analysis.Convex.Deriv public import Mathlib.Analysis.Calculus.MeanValue public import Mathlib.Tactic.Linarith public import Mathlib.Tactic.FieldSimp public import Mathlib.Tactic.FunProp public import Mathlib.Tactic.GCongr public import Mathlib.Tactic.Ring public import Mathlib.Tactic.NormNum public import Mathlib.Tactic.Positivity public import Aesop
+// Imports: public import Init public import Mathlib.Analysis.SpecialFunctions.Log.Basic
 #include <lean/lean.h>
 #if defined(__clang__)
 #pragma clang diagnostic ignored "-Wunused-parameter"
@@ -14,18 +14,7 @@
 extern "C" {
 #endif
 lean_object* initialize_Init(uint8_t builtin);
-lean_object* initialize_mathlib_Mathlib_Analysis_SpecialFunctions_ExpDeriv(uint8_t builtin);
-lean_object* initialize_mathlib_Mathlib_Analysis_SpecialFunctions_Log_Deriv(uint8_t builtin);
-lean_object* initialize_mathlib_Mathlib_Analysis_Convex_Deriv(uint8_t builtin);
-lean_object* initialize_mathlib_Mathlib_Analysis_Calculus_MeanValue(uint8_t builtin);
-lean_object* initialize_mathlib_Mathlib_Tactic_Linarith(uint8_t builtin);
-lean_object* initialize_mathlib_Mathlib_Tactic_FieldSimp(uint8_t builtin);
-lean_object* initialize_mathlib_Mathlib_Tactic_FunProp(uint8_t builtin);
-lean_object* initialize_mathlib_Mathlib_Tactic_GCongr(uint8_t builtin);
-lean_object* initialize_mathlib_Mathlib_Tactic_Ring(uint8_t builtin);
-lean_object* initialize_mathlib_Mathlib_Tactic_NormNum(uint8_t builtin);
-lean_object* initialize_mathlib_Mathlib_Tactic_Positivity(uint8_t builtin);
-lean_object* initialize_aesop_Aesop(uint8_t builtin);
+lean_object* initialize_mathlib_Mathlib_Analysis_SpecialFunctions_Log_Basic(uint8_t builtin);
 static bool _G_initialized = false;
 LEAN_EXPORT lean_object* initialize_GumbelLPP_GumbelLPP_GumbelLPP(uint8_t builtin) {
 lean_object * res;
@@ -34,40 +23,7 @@ _G_initialized = true;
 res = initialize_Init(builtin);
 if (lean_io_result_is_error(res)) return res;
 lean_dec_ref(res);
-res = initialize_mathlib_Mathlib_Analysis_SpecialFunctions_ExpDeriv(builtin);
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_mathlib_Mathlib_Analysis_SpecialFunctions_Log_Deriv(builtin);
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_mathlib_Mathlib_Analysis_Convex_Deriv(builtin);
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_mathlib_Mathlib_Analysis_Calculus_MeanValue(builtin);
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_mathlib_Mathlib_Tactic_Linarith(builtin);
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_mathlib_Mathlib_Tactic_FieldSimp(builtin);
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_mathlib_Mathlib_Tactic_FunProp(builtin);
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_mathlib_Mathlib_Tactic_GCongr(builtin);
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_mathlib_Mathlib_Tactic_Ring(builtin);
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_mathlib_Mathlib_Tactic_NormNum(builtin);
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_mathlib_Mathlib_Tactic_Positivity(builtin);
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_aesop_Aesop(builtin);
+res = initialize_mathlib_Mathlib_Analysis_SpecialFunctions_Log_Basic(builtin);
 if (lean_io_result_is_error(res)) return res;
 lean_dec_ref(res);
 return lean_io_result_mk_ok(lean_box(0));
